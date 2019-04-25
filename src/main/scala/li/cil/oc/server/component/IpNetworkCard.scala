8d21348bb1c2c8b397d8e74d05b1ab9558d4f4ba
Nick Avery <35745096+NickAvery@users.noreply.github.com>
2019-04-25 12:55:33 -0700

    Small changes and adding Functions
    Made some small changes to the ip network and took out the IpWirelessNetworkCard for now. Also added more functions to IpNetworkCard.

--- src/main/scala/li/cil/oc/server/component/IpNetworkCard.scala
+++ src/main/scala/li/cil/oc/server/component/IpNetworkCard.scala
@@ -1,39 +1,24 @@
 package li.cil.oc.server.component
 
-import java.util
-
-import com.google.common.base.Charsets
-import li.cil.oc.Constants
-import li.cil.oc.common.Tier
-import li.cil.oc.api.driver.DeviceInfo.DeviceAttribute
-import li.cil.oc.api.driver.DeviceInfo.DeviceClass
-import li.cil.oc.Settings
+import li.cil.oc.server.network.IpNetwork
 import li.cil.oc.api
-import li.cil.oc.api.component.RackBusConnectable
-import li.cil.oc.api.driver.DeviceInfo
-import li.cil.oc.api.internal.Rack
 import li.cil.oc.api.machine.Arguments
 import li.cil.oc.api.machine.Callback
 import li.cil.oc.api.machine.Context
-import li.cil.oc.api.network.EnvironmentHost
-import li.cil.oc.api.network._
-import li.cil.oc.server.network.IpNetwork
+import li.cil.oc.api.network.{EnvironmentHost, Node}
 import li.cil.oc.server.{PacketSender => ServerPacketSender}
-import net.minecraft.nbt._
 
-import scala.collection.convert.WrapAsJava._
 import scala.collection.convert.WrapAsScala._
-import scala.collection.mutable
+
 
 class IpNetworkCard(host : EnvironmentHost) extends NetworkCard(host)
 {
-  val ipAddress : String = "0.0.0.0"
-  val macAddress : String = "00:00:00:00"
+  var ipAddress : String = "0.0.0.0"
+  var macAddress : String = "00:00:00:00"
 
   @Callback(doc = """function() -- get the IP address for your IP Network Card""")
   def getIpAddress(context: Context, args: Arguments): Array[AnyRef] = {
-    result(IpNetwork.getIpAddress(node))
-
+    result(ipAddress)
   }
 
   @Callback(doc = """function() -- get the MAC address for your IP Network Card""")
@@ -42,89 +27,18 @@ class IpNetworkCard(host : EnvironmentHost) extends NetworkCard(host)
   }
 
   @Callback(doc = """function() -- ask for an IP Address from the network""")
-  def retrieveIpAddress(context: Context, args: Arguments): Array[AnyRef] = {
-    result(ipAddress)
-  }
-}
-
-/*
-class IpNetworkCard(host : EnvironmentHost) extends prefab.ManagedEnvironment with RackBusConnectable with DeviceInfo with traits.WakeMessageAware {
-  val ipAddress : String = ""
-
-  def getIpAddress(): String = {
-    ipAddress
-  }
-  protected val visibility = host match {
-    case _: Rack => Visibility.Neighbors
-    case _ => Visibility.Network
-  }
-
-  override val node = Network.newNode(this, visibility).
-    withComponent("modem", Visibility.Neighbors).
-    create()
-
-  protected val openPorts = mutable.Set.empty[Int]
-
-  // wired network card is the 1st in the max ports list (before both wireless cards)
-  protected def maxOpenPorts = Settings.get.maxOpenPorts(Tier.One)
-
-  // ----------------------------------------------------------------------- //
-
-  private final lazy val deviceInfo = Map(
-    DeviceAttribute.Class -> DeviceClass.Network,
-    DeviceAttribute.Description -> "Ethernet controller with IP/MAC addresses",
-    DeviceAttribute.Vendor -> Constants.DeviceInfo.DefaultVendor,
-    DeviceAttribute.Product -> "UI001",
-    DeviceAttribute.Version -> "1.0",
-    DeviceAttribute.Capacity -> Settings.get.maxNetworkPacketSize.toString,
-    DeviceAttribute.Size -> maxOpenPorts.toString,
-    DeviceAttribute.Width -> Settings.get.maxNetworkPacketParts.toString
-  )
-
-  override def getDeviceInfo: util.Map[String, String] = deviceInfo
-
-  // ----------------------------------------------------------------------- //
-
-  @Callback(doc = """function(port:number):boolean -- Opens the specified port. Returns true if the port was opened.""")
-  def open(context: Context, args: Arguments): Array[AnyRef] = {
-    val port = checkPort(args.checkInteger(0))
-    if (openPorts.contains(port)) result(false)
-    else if (openPorts.size >= maxOpenPorts) {
-      throw new java.io.IOException("too many open ports")
-    }
-    else result(openPorts.add(port))
+  def receiveIpAddress(context: Context, args: Arguments): Array[AnyRef] = {
+    var address = IpNetwork.receiveIpAddress(node.address())
+    ipAddress = address
+    address = IpNetwork.receiveMacAddress(node.address())
+    macAddress = address
+    result(ipAddress, macAddress)
   }
 
 
-
-  @Callback(doc = """function([port:number]):boolean -- Closes the specified port (default: all ports). Returns true if ports were closed.""")
-  def close(context: Context, args: Arguments): Array[AnyRef] = {
-    if (args.count == 0) {
-      val closed = openPorts.nonEmpty
-      openPorts.clear()
-      result(closed)
-    }
-    else {
-      val port = checkPort(args.checkInteger(0))
-      result(openPorts.remove(port))
-    }
-  }
-
-  @Callback(direct = true, doc = """function(port:number):boolean -- Whether the specified port is open.""")
-  def isOpen(context: Context, args: Arguments): Array[AnyRef] = {
-    val port = checkPort(args.checkInteger(0))
-    result(openPorts.contains(port))
-  }
-
-  @Callback(direct = true, doc = """function():boolean -- Whether this card has wireless networking capability.""")
-  def isWireless(context: Context, args: Arguments): Array[AnyRef] = result(false)
-
-  @Callback(direct = true, doc = """function():boolean -- Whether this card has wired networking capability.""")
-  def isWired(context: Context, args: Arguments): Array[AnyRef] = result(true)
-
-  @Callback(doc = """function(address:string, port:number, data...) -- Sends the specified data to the specified target.""")
-  def send(context: Context, args: Arguments): Array[AnyRef] = {
-    val address = args.checkString(0)
+  @Callback(doc = """function(address:string, port:number, data...) -- Sends data to the specified IP Address and port number.""")
+  def sendViaIp(context: Context, args: Arguments): Array[AnyRef] = {
+    val address = IpNetwork.uuidToIp(args.checkString(0))
     val port = checkPort(args.checkInteger(1))
     val packet = api.Network.newPacket(node.address, address, port, args.drop(2).toArray)
     doSend(packet)
@@ -132,82 +46,19 @@ class IpNetworkCard(host : EnvironmentHost) extends prefab.ManagedEnvironment wi
     result(true)
   }
 
-  @Callback(doc = """function(port:number, data...) -- Broadcasts the specified data on the specified port.""")
-  def broadcast(context: Context, args: Arguments): Array[AnyRef] = {
-    val port = checkPort(args.checkInteger(0))
-    val packet = api.Network.newPacket(node.address, null, port, args.drop(1).toArray)
-    doBroadcast(packet)
-    networkActivity()
-    result(true)
+  @Callback(doc = """function() -- free the IP address associated with your IP Network Card""")
+  def freeAddress(context: Context, args: Arguments): Array[AnyRef] = {
+    IpNetwork.freeAddress(node.address)
+    result("addresses have been freed!")
   }
-
-  //Removed in MC 1.11
-  @Callback(direct = true, doc = """function():number -- Gets the maximum packet size (config setting).""")
-  def maxPacketSize(context: Context, args: Arguments): Array[AnyRef] = result(Settings.get.maxNetworkPacketSize)
-
-  protected def doSend(packet: Packet) = visibility match {
-    case Visibility.Neighbors => node.sendToNeighbors("network.message", packet)
-    case Visibility.Network => node.sendToReachable("network.message", packet)
-    case _ => // Ignore.
-  }
-
-  protected def doBroadcast(packet: Packet) = visibility match {
-    case Visibility.Neighbors => node.sendToNeighbors("network.message", packet)
-    case Visibility.Network => node.sendToReachable("network.message", packet)
-    case _ => // Ignore.
-  }
-
-  // ----------------------------------------------------------------------- //
 
   override def onDisconnect(node: Node) {
     super.onDisconnect(node)
     if (node == this.node) {
       openPorts.clear()
     }
+    IpNetwork.freeAddress(node.address)
   }
-
-  override def onMessage(message: Message) = {
-    super.onMessage(message)
-    if ((message.name == "computer.stopped" || message.name == "computer.started") && node.isNeighborOf(message.source))
-      openPorts.clear()
-    if (message.name == "network.message") message.data match {
-      case Array(packet: Packet) => receivePacket(packet)
-      case _ =>
-    }
-  }
-
-  override protected def isPacketAccepted(packet: Packet, distance: Double): Boolean = {
-    if (super.isPacketAccepted(packet, distance)) {
-      if (openPorts.contains(packet.port)) {
-        networkActivity()
-        return true
-      }
-    }
-    false
-  }
-
-  override def receivePacket(packet: Packet): Unit = receivePacket(packet, 0, host)
-
-  // ----------------------------------------------------------------------- //
-
-  override def load(nbt: NBTTagCompound) {
-    super.load(nbt)
-    assert(openPorts.isEmpty)
-    openPorts ++= nbt.getIntArray("openPorts")
-    loadWakeMessage(nbt)
-  }
-
-  override def save(nbt: NBTTagCompound) {
-    super.save(nbt)
-    nbt.setIntArray("openPorts", openPorts.toArray)
-    saveWakeMessage(nbt)
-  }
-
-  // ----------------------------------------------------------------------- //
-
-  protected def checkPort(port: Int) =
-    if (port < 1 || port > 0xFFFF) throw new IllegalArgumentException("invalid port number")
-    else port
 
   private def networkActivity() {
     host match {
@@ -216,4 +67,3 @@ class IpNetworkCard(host : EnvironmentHost) extends prefab.ManagedEnvironment wi
     }
   }
 }
-*/
--- src/main/scala/li/cil/oc/server/network/IpNetwork.scala
+++ src/main/scala/li/cil/oc/server/network/IpNetwork.scala
@@ -2,19 +2,57 @@ package li.cil.oc.server.network
 
 import li.cil.oc.api.network
 
-
-
 object IpNetwork {
-                              //UUID    IP      MAC
-  protected var addresses : Map[String, Array[String]] = null
+  // uuid to ip address mapping
+  protected var ipAddresses : Map[String, String] = Map.empty
 
+  //uuid to mac address mapping
+  protected var macAddresses : Map[String, String] = Map.empty
+
+  //bad way to create more ip and mac addresses
+  private var nextIpAddress : Int = 0
+
+  private var nextMacAddress : Int = 0
+
+  //get the ip address of the node passed in. Don't know why it cant be a Node
   def getIpAddress(node:network.Component) = {
     uuidToIp(node.address)
   }
 
+  //get the ip address for a uuid and vice versa if such a string exists
   def uuidToIp(address: String) = {
-    //addresses(address)(0)
-    "0.0.0.0"
+    ipAddresses(address)
   }
+
+  //remove all mappings associated with mac and ipaddresses to uuid
+  def freeAddress(address: String) = {
+    val mac = macAddresses(address)
+    val ip = ipAddresses(address)
+    macAddresses -= (mac, address)
+    ipAddresses -= (ip, address)
+  }
+
+  //get an ip address for a uuid
+  def receiveIpAddress(address: String) = {
+    val mapping = "0.0.0."+nextIpAddress
+    nextIpAddress += 1
+    //allows for easy lookup between addresses and makes sure they are unique
+    ipAddresses += (address -> mapping)
+    ipAddresses += (mapping -> address)
+    ipAddresses(address)
+  }
+
+  //get a mac address for a uuid
+  def receiveMacAddress(address: String) = {
+    //really bad way of creating addresses... but a place holder for now
+    val mapping = "00:00:00:0"+nextMacAddress
+    nextMacAddress += 1
+    macAddresses += (address -> mapping)
+    macAddresses += (mapping -> address)
+    macAddresses(address)
+  }
+
+  //things to do
+  //set your own ip address
 
 }
